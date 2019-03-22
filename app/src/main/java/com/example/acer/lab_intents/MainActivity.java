3d5f765051c299527bac7b50b87b0b36366fb41f
EdwinLovo <00120617@uca.edu.sv>
2019-03-22 10:44:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/acer/lab_intents/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.acer.lab_intents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.acer.lab_intents.utils.AppConstant;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEdit;
+    private Button mButton, mButtonShare;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEdit = findViewById(R.id.et_text);
+        mButton = findViewById(R.id.btn_send);
+        mButtonShare = findViewById(R.id.btn_share);
+
+        mButton.setOnClickListener(v->{
+            String text = mEdit.getText().toString();
+            Intent mIntent = new Intent(MainActivity.this, NewActivity.class);
+            mIntent.putExtra(AppConstant.TEXT_KEY,text);
+            startActivity(mIntent);
+        });
+
+        mButtonShare.setOnClickListener(v->{
+            String text = mEdit.getText().toString();
+            Intent mIntent = new Intent();
+            mIntent.setType("text/plain");
+            mIntent.setAction(Intent.ACTION_SEND);
+            mIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(mIntent);
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/acer/lab_intents/utils/AppConstant.java
@@ -0,0 +1,5 @@
+package com.example.acer.lab_intents.utils;
+
+public class AppConstant {
+    public static final String TEXT_KEY = "textKey";
+}
